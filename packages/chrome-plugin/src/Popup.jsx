e015460e866ccf8c1fbb690282fac807486673a7
ByteDance <zhangyunnian@bytedance.com>
2023-09-13 01:20:17 +0800

    modify

--- packages/chrome-plugin/src/Popup.jsx
+++ packages/chrome-plugin/src/Popup.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default () => {
     return (
-        <div className="w:360 h:560 p:r">
-            <div className="h:100% pb:56" style={{ boxSizing: 'border-box' }}>
-                <div className="h:100% bc:ddd bs:s bw:1 br:2 p:10">
+        <div className="w:360 d:f fd:c" style={{ boxSizing: "border-box" }}>
+            <div className="h:520 bc:ddd bs:s bw:1 br:4 p:8 mb:10">
     44432
-                </div>
             </div>
-            <div className="d:f ai:c jc:sb p:f b:10 l:0 w:100%">
-                <input className="w:100% pl:8 pr:8 lh:20 ml:10" />
-                <button className="w:120 ml:8 h:26 mr:10">发送</button>
+            <div className="d:f ai:c jc:sb w:100%">
+                <input className="w:100% pl:8 pr:8 lh:20" />
+                <button className="w:120 ml:8 h:26">发送</button>
             </div>
         </div>
     )
