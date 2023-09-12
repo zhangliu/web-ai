c3c8947771a700cfb946db5c07e4ba3596be4bc6
ByteDance <zhangyunnian@bytedance.com>
2023-09-13 07:19:14 +0800

    modify

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const Koa = require('koa');
 const routes = require('./src/routes');
+const cors = require('koa2-cors');
 
 const port = 3033;
 const app = new Koa();
 
 app.use(routes);
+// see: https://github.com/zadzbw/koa2-cors
+app.use(cors({
+    origin: (ctx) => ctx.request.header.origin,
+}));
 
 // 启动服务器
 app.listen(port, () => {
