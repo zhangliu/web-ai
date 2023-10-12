f1fa357694cecb5c83f0b0c50366c9bab8e32059
ByteDance <zhangyunnian@bytedance.com>
2023-10-12 22:46:26 +0800

    modify

--- src/utils/aiNg/index.js
+++ src/utils/aiNg/index.js
@@ -19,6 +19,7 @@ ng.prompt = async (prompt) => {
         .type('footer textarea', prompt)
         .click('footer > div > div > button:last-child')
         .evaluate(async () => {
+            let startTime = Date.now();
             while(true) {
                 await window.ng.utils.sleep(500);
                 const footer = document.querySelector('footer');
@@ -26,6 +27,8 @@ ng.prompt = async (prompt) => {
                 const answerNode = children[1];
                 if (!answerNode) continue;
                 if (answerNode.getAttribute('data-complete') !== 'true') continue;
+                // gpt 回答太快，可能错误的用了历史的数据
+                if (Date.now() - startTime <= 2000) continue;
                 return answerNode.children[1].innerText;
             }
         });
