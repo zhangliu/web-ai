39849f1e157d6f964193fb0ebaae4b172c948a9d
ByteDance <zhangyunnian@bytedance.com>
2023-10-16 13:16:24 +0800

    modify

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -2,13 +2,16 @@ const { getChatBot } = require('../handlers/aiHandler');
 const logger = require('../utils/logger');
 
 const chatMap = {
-    '*': '2ms7cmxdagq3r4dpy3h'
-}
+    '*': '2ms7cmxdagq3r4dpy3h',
+    'Ai助手测试群': '2ms7cmxdagq3r4dpy3h',
+};
   
 async function getAnswer(ctx) {
-    const { question, userId} = ctx.query;
+    const { question, userId, isGroup} = ctx.query;
     if (!question) throw new Error('Need Question!');
+    if (!userId) throw new Error('Need userId!');
 
+    logger.info(`will handle for user: ${userId}, question: ${question}`);
     const aiBot = (await getChatBot(chatMap[userId] || chatMap[0]));
     try {
         const answer = await aiBot.prompt(question);
