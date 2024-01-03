7f7a3e98efe9dba47c28f1ce5e65255c3e9e9365
张云念 <zhangyunnian@bytedance.com>
2024-01-03 12:40:22 +0800

    modify

---
+++ scripts/initAws.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# 安装 node 16
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 16
+nvm use 16
+
+# 安装 docker
+sudo snap install docker
+chmod 666 /var/run/docker.sock
+
