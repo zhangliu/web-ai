38a517a6aa6c57a5bcf4c46f1ce56819c274ffb2
张云念 <zhangyunnian@bytedance.com>
2024-01-17 12:45:05 +0800

    modify

--- scripts/initAws.sh
+++ scripts/initAws.sh
@@ -7,7 +7,11 @@ source ~/.bashrc
 nvm install 16
 nvm use 16
 
+# 安装 puppeteer 的环境依赖
+apt install -y libnss3 libgtk-3-0 libasound2
+sudo apt install -y libgbm1
+
 # 安装 docker
-sudo snap install docker
-sudo chmod 666 /var/run/docker.sock
+# sudo snap install docker
+# sudo chmod 666 /var/run/docker.sock
 
